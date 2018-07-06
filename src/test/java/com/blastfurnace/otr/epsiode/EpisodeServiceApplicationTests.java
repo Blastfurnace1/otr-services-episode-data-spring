9f073b0ab5778835de717d590e5bf01fac20a990
James B Blackson <jamesblackson880@gmail.com>
2018-07-05 21:33:52 -0400

    fix parent class

--- src/test/java/com/blastfurnace/otr/epsiode/EpisodeServiceApplicationTests.java
+++ src/test/java/com/blastfurnace/otr/epsiode/EpisodeServiceApplicationTests.java
@@ -1,30 +1,11 @@
-/*
- * Copyright 2012-2014 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.blastfurnace.otr.epsiode;
 
 
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 
-import com.blastfurnace.otr.utils.UtilitiesApplicationTest;
+import com.blastfurnace.otr.AppConfigTest;
 
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.junit.Assert.assertTrue;
@@ -34,7 +15,7 @@ import static org.junit.Assert.assertTrue;
  *
  * @author Jim Blackson
  */
-public class EpisodeServiceApplicationTests extends UtilitiesApplicationTest {
+public class EpisodeServiceApplicationTests extends AppConfigTest {
 	
 	private static final Logger log = LoggerFactory.getLogger(EpisodeServiceApplicationTests.class); 
 
